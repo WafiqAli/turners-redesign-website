73cc8f6de4133ebaf86d3e1a9a17b3c09b247ffb
WafiqAli <wafiqcali@gmail.com>
2022-05-27 12:10:54 +1200

    added some fake testimonial reviews for fun

--- src/components/TestimonialTabs/testimonials.js
+++ src/components/TestimonialTabs/testimonials.js
@@ -17,9 +17,24 @@ export const testimonials = [
                 comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
             },
             {
-                name: "Mitch Janes",
+                name: "Janes Mitch",
                 date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
+                comment: "I am happy with the service. Good stuff guys"
+            },
+            {
+                name: "Khada Jhin",
+                date: "April 4444",
+                comment: "Turners most certainly performed wonderfully. And now, once again, the curtain shall rise!"
+            },
+            {
+                name: "Heimerdinger",
+                date: "September 1234",
+                comment: "I am glad that turners was able to fix my car. This second sentence is written purely to fill up this card. This is my third and last sentence, thank you :)"
+            },
+            {
+                name: "Nathan Drake",
+                date: "May 2016",
+                comment: "Thank god Turners was able to fix my Jeep. I thought it was a goner after taking it through that jungle. I have no idea what else to say. Great work!"
             },
             {
                 name: "Mitch Janes",
@@ -27,29 +42,14 @@ export const testimonials = [
                 comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
             },
             {
-                name: "Mitch Janes",
-                date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
+                name: "Ping",
+                date: "August 2020",
+                comment: "Pong!"
             },
             {
-                name: "Mitch Janes",
+                name: "Sir Francis Drake",
                 date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
-            },
-            {
-                name: "Mitch Janes",
-                date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
-            },
-            {
-                name: "Mitch Janes",
-                date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
-            },
-            {
-                name: "Mitch Janes",
-                date: "May 2021",
-                comment: "Really happy with Turner's, manager went out of their way to greet us and made sure we were looked after. Staff were really friendly and the dealership had a good selection and atmosphere."
+                comment: "This is setence one. This is sentence two. This is sentence three. Thank you Turners cars for fixing my SUV :D"
             }
 
         ]
